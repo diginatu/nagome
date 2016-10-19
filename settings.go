b8caaf9b7464bf5b6242da6339006ae089a5df65
diginatu <diginatu.510o@gmail.com>
2016-10-20 00:52:42 +0900

    add settings struct

---
+++ settings.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+// A SettingsSlot represents a settings of Nagome.
+type SettingsSlot struct {
+	UserNameGet bool `yaml:"user_name_get" json:"user_name_get"`
+}
+
+// Equal reports whether c and x represent the same SettingsSlot instant.
+func (c *SettingsSlot) Equal(x *SettingsSlot) bool {
+	return c.UserNameGet == x.UserNameGet
+}
+
+// SettingsSlots is struct for multiple configs file.
+type SettingsSlots struct {
+	Config []*SettingsSlot `yaml:"config" json:"config"`
+}
+
+// Add adds given slot to the list.
+func (ss *SettingsSlots) Add(s *SettingsSlot) {
+	ss.Config = append(ss.Config, s)
+}
+
+// Save saves to a file.
+func (ss *SettingsSlots) Save() error {
+	s, err := yaml.Marshal(ss)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(filepath.Join(App.SavePath, settingsFileName), s, 0600)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Load loads from a file.
+func (ss *SettingsSlots) Load() error {
+	f, err := ioutil.ReadFile(filepath.Join(App.SavePath, settingsFileName))
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(f, ss)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ settings_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSettingsSlots(t *testing.T) {
+	App.SavePath = os.TempDir()
+
+	ss := SettingsSlots{}
+	testSetting := &SettingsSlot{
+		UserNameGet: false,
+	}
+	testSetting2 := &SettingsSlot{
+		UserNameGet: true,
+	}
+	ss.Add(testSetting)
+	ss.Add(testSetting2)
+
+	err := ss.Save()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(filepath.Join(App.SavePath, settingsFileName))
+
+	err = ss.Load()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got := len(ss.Config); got != 2 {
+		t.Fatalf("Should be %v but %v", 2, got)
+	}
+	if got := ss.Config[0]; !got.Equal(testSetting) {
+		t.Fatalf("Should be %v but %v", testSetting, got)
+	}
+	if got := ss.Config[1]; !got.Equal(testSetting2) {
+		t.Fatalf("Should be %v but %v", testSetting2, got)
+	}
+}
